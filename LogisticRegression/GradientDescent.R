ca8cb2d10c982c416386d8dd9c7fff8e7beefdab
JHonaker <honaker215@gmail.com>
2014-05-19 14:04:04 -0400

    Correctly calculated gradient (dJ) for logitstic.

--- LogisticRegression/GradientDescent.R
+++ LogisticRegression/GradientDescent.R
@@ -4,17 +4,17 @@
 sigmoid <- function(x) 1 / (1 + exp(-x))
 
 cost <- function(z, y, h=sigmoid) {
-  # This creates a convex function that we can optimize over. 
-  # It can be derived using maximum likelihood estimation.
-  -(y * log(h(z)) + (1-y) * log(1 - h(z)))
+    # This creates a convex function that we can optimize over. 
+    # It can be derived using maximum likelihood estimation.
+    -(y * log(h(z)) + (1-y) * log(1 - h(z)))
 }
 
 J <- function(theta, X, Y) {
-  if (!is.matrix(X)) X <- as.matrix(X)
-  if (!is.matrix(theta)) theta <- as.matrix(theta)
-  if (!is.matrix(Y)) Y <- as.matrix(Y)
-  
-  1 / nrow(X) * sum(cost(X %*% theta , Y))
+    if (!is.matrix(X)) X <- as.matrix(X)
+    if (!is.matrix(theta)) theta <- as.matrix(theta)
+    if (!is.matrix(Y)) Y <- as.matrix(Y)
+    
+    1 / nrow(X) * sum(cost(X %*% theta , Y))
 }
 
 # To fit the regression coefficients we must minimize the cost, J(theta: (X,Y))
@@ -23,21 +23,34 @@ J <- function(theta, X, Y) {
 # where alpha is the specified "learning rate".
 
 # To do this we will need the derivative of J(theta) wrt. theta_j
-dJ <- function(theta, X, Y) {
-  if (!is.matrix(X)) X <- as.matrix(X)
-  if (!is.matrix(theta)) theta <- as.matrix(theta)
-  if (!is.matrix(Y)) Y <- as.matrix(Y)
-  
-  1 / nrow(X) * t(X) %*% cost(X %*% theta , Y)
+dJ <- function(theta, X, Y, h=sigmoid) {
+    if (!is.matrix(X)) X <- as.matrix(X)
+    if (!is.matrix(theta)) theta <- as.matrix(theta)
+    if (!is.matrix(Y)) Y <- as.matrix(Y)
+    
+    1 / nrow(X) * t(X) %*% (h(X %*% theta) - Y)
 }
 
 update <- function(theta, X, Y, alpha=0.05) {
-  theta <- theta - alpha * dJ(theta, X, Y)
-  theta
+    theta <- theta - alpha * dJ(theta, X, Y)
+    theta
 }
 
-solution <- matrix(NA, 2, 1000)
-solution[,1] <- matrix(1,2,1)
-for (i in 2:1000) {
-  solution[,i] <- update(solution[,i-1], x, y)
+sz <- 1000
+x <- cbind( 1, rnorm(sz))
+theta <- c(2, 3)
+z <- x %*% theta
+pr <- sigmoid(z)
+y <- rbinom(sz, 1, pr)
+
+iters <- 10000
+solution <- matrix(NA, 2, iters)
+Jmat <- rep(NA, iters)
+dJmat <- rep(NA, iters)
+solution[,1] <- as.matrix(c(1, 1))
+Jmat[1] <- J(solution[,1], x, y)
+for (i in 2:iters) {
+    solution[,i] <- update(solution[,i-1], x, y)
+    Jmat[i] <- J(solution[,i], x, y)
 }
+plot(1:iters, Jmat)
