9df13229b974c74c6b9e01c60c33b87c527c8e7e
JHonaker <honaker215@gmail.com>
2014-05-28 18:27:48 -0400

    Started ID3 Decision Tree algorithm

---
+++ DecisionTree/ID3.R
@@ -0,0 +1,24 @@
+# ID3 or 'Iterative Dichotomizer 3' is and algorithm for creating
+# a decision tree from a dataset. It is fairly simple, and we will
+# use this as an introduction to decision tree learning.
+
+# First, while not strictly necessary, we will create two R class types:
+# Tree and Node for easier handling of the tree-structure.
+
+# Tree:
+# Tree will be used in a recursive structure that contians a either
+# another tree or a node at each branch. Notice that we use a list
+# for the branches, so that each tree can contain an arbitrary number
+# of brances.
+tree <- function(val, branch.list) {
+
+}
+# Node:
+# Node is the used for the terminal location in the trees. Each branch
+# that contains a node will signify that the algorithm has either:
+# 	1. Every element in the subset belongs to the same class
+#	2. There are no more attributes to be selected
+#	3. There are no more examples in the subset
+node <- function(val) {
+
+}
